adad089c35d099a859508a209a7c0b1a501bf906
Jone23 <jone23@ramble.ru>
2022-03-31 06:47:25 +0300

    Добавил в класс AnimatedColoredTextView: Методы: --initContainer(ConstraintLayout container)-для инициализации ConstraintLayout,для обработке касания по ConstraintLayout. --initHelloTextView(TextView textView)-для инициализации TextView, для добавления ссылке на TextView и обработки клика по тексту. --startAnimation(float sizeOfTheMovingArea)-для реализации задержки в 5 сек. и анимированного перемещения текста

--- app/src/main/java/com/example/animatedcoloredtextview/AnimatedColoredTextView.java
+++ app/src/main/java/com/example/animatedcoloredtextview/AnimatedColoredTextView.java
@@ -5,51 +5,135 @@ import android.graphics.Color;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
-import java.util.Locale;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class AnimatedColoredTextView {
 
     private Context context;
-
     private TextView animatedColoredTextView;
+    private Timer timerAnimation;
 
-    AnimatedColoredTextView(Context context, TextView textView)
+    private boolean downMove=true;
+    private boolean firstActivationOfAnimation=false;
+
+    AnimatedColoredTextView(Context context)
     {
         this.context=context;
-
+    }
+    public void  initHelloTextView(TextView textView)
+    {
         animatedColoredTextView=textView;
+
+        animatedColoredTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timerAnimation.cancel();
+            }
+        });
     }
 
+    public void  initContainer(ConstraintLayout container)
+    {
+        container.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if(firstActivationOfAnimation)timerAnimation.cancel();
+
+                setColorEnglishAndRussianLocales(Color.RED,Color.BLUE);
+
+                startAnimation(container.getHeight());
+
+                move(motionEvent.getX(),motionEvent.getY());
+                return false;
+            }
+        });
+    }
     public void move(float x, float y)
     {
-        animatedColoredTextView.setX(x);
-        animatedColoredTextView.setY(y);
+        animatedColoredTextView.setX(x-animatedColoredTextView.getWidth()/2);
+        animatedColoredTextView.setY(y-animatedColoredTextView.getHeight()/2);
     }
 
     public void setText(String text)
     {
-        animatedColoredTextView.setText(text);
+        animatedColoredTextView.setText(text.toUpperCase());
     }
 
-    public void setColorEnglishAndRussianLocales(int colorEnglishLocale, int colorRussianLocale)
+    private void setColorEnglishAndRussianLocales(int colorEnglishLocale, int colorRussianLocale)
     {
-        animatedColoredTextView.getText();
-        animatedColoredTextView.setTextColor(colorRussianLocale);
+        if(animatedColoredTextView.getCurrentTextColor()==Color.WHITE) {
 
-        String localesEnglish;
-        localesEnglish=(String)animatedColoredTextView.getText();
-        localesEnglish=localesEnglish.toUpperCase();
+            animatedColoredTextView.setTextColor(colorRussianLocale);
 
-        int numberOfEnglishletters=localesEnglish.replaceAll("[А-Я\\d]","").length();
+            String localesEnglish;
+            localesEnglish = (String) animatedColoredTextView.getText();
 
-        Spannable spannable= new SpannableString(localesEnglish);
-        spannable.setSpan(new ForegroundColorSpan(colorEnglishLocale),0,numberOfEnglishletters,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            int numberOfEnglishletters = localesEnglish.replaceAll("[А-Я\\d]", "").length();
 
-        animatedColoredTextView.setText(spannable);
+            Spannable spannable = new SpannableString(localesEnglish);
+            spannable.setSpan(new ForegroundColorSpan(colorEnglishLocale), 0, numberOfEnglishletters, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            animatedColoredTextView.setText(spannable);
+        }
+    }
+
+    private void startAnimation(float sizeOfTheMovingArea)
+    {
+        firstActivationOfAnimation=true;
+        TranslateAnimation animationPause=new TranslateAnimation(
+                Animation.ABSOLUTE, 0,
+                Animation.ABSOLUTE, 0,
+                Animation.ABSOLUTE, 0,
+                Animation.ABSOLUTE, 0);
+        animationPause.setDuration(5000);
+
+        animationPause.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                timerAnimation=new Timer(true);
+                timerAnimation.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+
+                        if(animatedColoredTextView.getY()+animatedColoredTextView.getHeight()<sizeOfTheMovingArea&&downMove)
+                        {animatedColoredTextView.setY(animatedColoredTextView.getY()+10);}
+                        else{downMove=false;}
+
+                        if(animatedColoredTextView.getY()>=0&&downMove==false) {
+                            animatedColoredTextView.setY(animatedColoredTextView.getY() - 10);}
+                        else{downMove=true;}
+                    }
+                },0,50);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        animatedColoredTextView.startAnimation(animationPause);
+
     }
 
 
 
 }
+
--- app/src/main/java/com/example/animatedcoloredtextview/MainActivity.java
+++ app/src/main/java/com/example/animatedcoloredtextview/MainActivity.java
@@ -5,8 +5,15 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +22,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AnimatedColoredTextView animatedColoredTextView=new AnimatedColoredTextView(this,findViewById(R.id.helloTextView));
+        AnimatedColoredTextView animatedColoredTextView=new AnimatedColoredTextView(this);
+        animatedColoredTextView.initHelloTextView(findViewById(R.id.helloTextView));
+        animatedColoredTextView.initContainer(findViewById(R.id.container));
         animatedColoredTextView.setText("hello привет");
-        animatedColoredTextView.setColorEnglishAndRussianLocales(Color.RED,Color.BLUE);
 
 
-        ConstraintLayout container=(ConstraintLayout)findViewById(R.id.container);
 
-        
+
+
+
+
 
     }
+
 }
